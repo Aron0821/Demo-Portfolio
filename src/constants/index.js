50e2d8c14530889564264edefa4038520851ecac
Aron0821 <aronshakha101@gmail.com>
2024-07-21 15:35:22 +0545

    Changes

---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate front-end developer currently learning and practicing React.js. I have a strong foundation in HTML, CSS, JavaScript, and Tailwind. My goal is to leverage my skills to create efficient and visually appealing web applications.`;
+
+export const ABOUT_TEXT = `I am a dedicated front-end developer in the learning stage with a passion for creating user-friendly web applications. My journey in web development began with a curiosity about how things work on the web, and I am currently focusing on mastering React.js. I thrive in environments where I can continuously learn and improve. Outside of coding, I enjoy exploring new technologies and staying active.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Learning Front-End Development",
+    company: "Self-Studying",
+    description: `Currently focused on learning front-end development with an emphasis on React.js. Practicing by building small projects and continuously improving my skills in HTML, CSS, JavaScript, and Tailwind.`,
+    technologies: ["HTML", "CSS", "JavaScript", "Tailwind", "React"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "Baniyatar, Kathmandu",
+  phoneNo: "+977 9840860625 ",
+  email: "aronshakha101@gmail.com",
+};
