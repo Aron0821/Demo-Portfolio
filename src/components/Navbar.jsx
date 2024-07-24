a2798d33179445c433472b21b8b873e2a8921a79
Aron0821 <aronshakha101@gmail.com>
2024-07-24 17:07:39 +0545

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,18 @@ const Navbar = () => {
         <h1 className="text-slate-100 text-4xl">As</h1>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a href="https://www.linkedin.com/in/aron-shakha-489051261/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Aron0821">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/aronshakha/">
+          <FaInstagram />
+        </a>
+        <a href="https://x.com/Arma1095">
+          <FaSquareXTwitter />
+        </a>
       </div>
     </nav>
   );
